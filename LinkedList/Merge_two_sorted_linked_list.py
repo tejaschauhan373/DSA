44ceb3caa77c05697b2d81258bb7527e9951e5a2
Tejas Chauhan <tejaschauhan373@gmail.com>
2021-08-01 18:03:35 +0530

    Added Merge two sorted Linked List

---
+++ LinkedList/Merge_two_sorted_linked_list.py
@@ -0,0 +1,32 @@
+class ListNode:
+
+    def __int__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def mergeTwoLinkeeList(l1: ListNode, l2: ListNode) -> ListNode:
+    if l1 is None:
+        return l2
+
+    if l2 is None:
+        return l1
+
+    if l1.val > l2.val:
+        temp = l1
+        l1 = l2
+        l2 = temp
+
+    res = l1
+    while l1 and l2:
+        temp = ListNode()
+        while l1 is not None and l1.val l < l2.val:
+            temp = l1
+            l1 = l1.next
+
+        temp = ListNode()
+        temp = l1
+        l1 = l2
+        l2 = temp
+
+    return res
