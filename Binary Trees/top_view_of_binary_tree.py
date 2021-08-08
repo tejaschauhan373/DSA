283093ca2bba535b7ef12d0816090a81f1f3df8e
Tejas Chauhan <tejaschauhan373@gmail.com>
2021-08-09 00:57:04 +0530

    [Binary Tree] top and bottom side view

---
+++ Binary Trees/top_view_of_binary_tree.py
@@ -0,0 +1,23 @@
+def top_view(root):
+    glo_d = {}
+
+    que = [root]
+    root.hd = 0
+    while que:
+        root = que[0]
+        hd = root.hd
+
+        if hd not in glo_d:
+            glo_d[hd] = root.data
+
+        if (root.left):
+            root.left.hd = hd - 1
+            que.append(root.left)
+
+        if (root.right):
+            root.right.hd = hd + 1
+            que.append(root.right)
+
+        que.pop(0)
+
+    return [glo_d[i] for i in sorted(glo_d.keys())]
