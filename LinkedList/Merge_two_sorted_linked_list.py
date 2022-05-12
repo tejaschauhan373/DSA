da94eaabfc3b3177caa0257e5308d96f8a007cd7
Tejas Chauhan <tejaschauhan373@gmail.com>
2022-05-12 23:58:35 +0530

    [LinkedList] Added problems of LinkedList

--- LinkedList/Merge_two_sorted_linked_list.py
+++ LinkedList/Merge_two_sorted_linked_list.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/771/
 class ListNode:
 
     def __int__(self, val=0, next=None):
@@ -5,7 +6,68 @@ class ListNode:
         self.next = next
 
 
-def merge_two_linked_list(l1: ListNode, l2: ListNode) -> ListNode:
+def merge_two_linked_list_optimal_one(l1: ListNode, l2: ListNode) -> ListNode:
+    """
+    Time Complexity = O(N + M)
+    Space Complexity = O(N + M) # to store recursive call stack
+    """
+
+    # base case
+    if l1 is None:
+        return l2
+
+    if l2 is None:
+        return l1
+
+    # recursive case
+    c = None
+
+    if l1.val > l2.val:
+        c = l1
+        c.next = merge_two_linked_list_optimal_one(l1.next, l2)
+    else:
+        c = l2
+        c.next = merge_two_linked_list_optimal_one(l1, l2.next)
+    return c
+
+
+def merge_two_linked_list_optimal_two(l1: ListNode, l2: ListNode) -> ListNode:
+    """
+    Time Complexity = O(N + M)
+    Space Complexity = O(1)
+    """
+
+    if l1 is None:
+        return l2
+
+    if l2 is None:
+        return l1
+
+    c = ListNode()
+    temp = c
+    while l1 and l2:
+
+        if l1.val < l2.val:
+            c.next = l1
+            l1 = l1.next
+        else:
+            c.next = l2
+            l2 = l2.next
+        c = c.next
+
+    if l1 is None:
+        c.next = l2
+    else:
+        c.next = l1
+
+    return temp.next
+
+
+def merge_two_linked_list_optimal_three(l1: ListNode, l2: ListNode) -> ListNode:
+    """
+    Time Complexity = O(N+M)
+    Space Complexity = O(1)
+    """
     if l1 is None:
         return l2
 
--- LinkedList/check_palindromic_linked_list.py
+++ LinkedList/check_palindromic_linked_list.py
@@ -1,12 +1,41 @@
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/772/
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 
-class Solution:
+def check_palindrome_naive(head):
+    """
+    Time Complexity = O(N)
+    Space Complexity = O(N)
+    """
+    if head is None:
+        return True
+    stack = []
+    first = head
+    while first:
+        stack.append(first.val)
+        first = first.next
 
-    def reverseList(self, head):
+    i = 0
+    last = len(stack) - 1
+
+    while i < last:
+        if stack[i] != stack[last]:
+            return False
+        i += 1
+        last -= 1
+    return True
+
+
+def check_palindrome_optimal(head):
+    """
+    Time Complexity = O(N)
+    Space Complexity = O(1)
+    """
+
+    def revers_list(head):
         pre = None
 
         while head:
@@ -17,27 +46,25 @@ class Solution:
 
         return pre
 
-    def isPalindrome(self, head: ListNode) -> bool:
+    if head is None:
+        return True
+    elif head.next is None:
+        return True
 
-        if head is None:
-            return True
-        elif head.next is None:
-            return True
+    fast = head
+    slow = head
 
-        fast = head
-        slow = head
-
-        while fast.next and fast.next.next:
-            fast = fast.next.next
-            slow = slow.next
-
-        slow.next = self.reverseList(slow.next)
+    while fast.next and fast.next.next:
+        fast = fast.next.next
         slow = slow.next
 
-        while slow:
-            if slow.val != head.val:
-                return False
-            slow = slow.next
-            head = head.next
+    slow.next = revers_list(slow.next)
+    slow = slow.next
 
-        return True
+    while slow:
+        if slow.val != head.val:
+            return False
+        slow = slow.next
+        head = head.next
+
+    return True
