91f01525342c54b896dedef8bb02779483f375a0
Tejas Chauhan <tejaschauhan373@gmail.com>
2022-05-28 11:29:33 +0530

    [Linked List] - add two number of linked list

--- LinkedList/add two number of linked list.py
+++ LinkedList/add two number of linked list.py
@@ -1,11 +1,12 @@
 # Definition for singly-linked list.
+# https://leetcode.com/problems/add-two-numbers/
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 
-def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+def add_two_numbers(l1: ListNode, l2: ListNode) -> ListNode:
     i = 0
 
     if l1.val == 0 and l2.next is None and l2.val == 0 and l1.next is None:
