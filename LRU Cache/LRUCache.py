4cfd5ff8ba834c36141c14abcd2b732de36b2d68
Tejas Chauhan <tejaschauhan373@gmail.com>
2022-06-28 21:08:13 +0530

    [LRU Cache] Updated with OrderedDict

--- LRU Cache/LRUCache.py
+++ LRU Cache/LRUCache.py
@@ -1,59 +1,38 @@
-from collections import deque
+from collections import OrderedDict
 
 
-# Node to store the data (Linked List)
-class Node:
-
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-
-
-# LRU Cache Data Structure
 class LRUCache:
+    def __init__(self, Capacity):
+        self.size = Capacity
+        self.cache = OrderedDict()
 
-    def __init__(self, max_size):
-        self.max_size = max(max_size, 1)
-        self.l = deque()
-        self.m = {}
-
-    def insert_key_value(self, key, value):
+    def get(self, key):
         """
         Time Complexity = O(1)
+        Space Complexity = O(1)
         """
-        # 2 cases
-        if key in self.m:
-            # replace the old value and update
-            it = self.m[key]
-            it.value = value
-        else:
-            # check if cache is full or not
-            # remove the least recently used item from cache
-            if len(self.l) == self.max_size:
-                last = self.l[-1]
-                del self.m[last.key]  # remove from hash map
-                self.l.pop()  # remove from linked list
+        if key not in self.cache: return None
+        val = self.cache[key]
+        self.cache.move_to_end(key)
+        return val
 
-            n = Node(key, value)
-            self.l.appendleft(n)
-            self.m[key] = n
-
-    def get_value(self, key):
+    def put(self, key, val):
         """
         Time Complexity = O(1)
+        Space Complexity = O(1)
         """
-        if key in self.m:
-            it = self.m[key]
-            value = it.value
-            self.l.remove(it)
-            self.l.appendleft(it)
-            return self.l[0].value
-        return None
+        if key in self.cache: del self.cache[key]
+        self.cache[key] = val
+        if len(self.cache) > self.size:
+            self.cache.popitem(last=False)
 
     def most_recent_key(self):
         """
-        Time Complexity = O(1)
+        Time Complexity = O(N)
+        Space Complexity = O(N)
         """
-        if self.l:
-            return self.l[0].key
-        return None
+        if self.cache:
+            it = next(reversed(self.cache))
+            return it
+        else:
+            return None
--- LRU Cache/lru_cache_operation.py
+++ LRU Cache/lru_cache_operation.py
@@ -2,28 +2,29 @@ from LRUCache import LRUCache
 
 if __name__ == "__main__":
     lru_cache = LRUCache(3)
-    lru_cache.insert_key_value("mango", 10)
-    lru_cache.insert_key_value("apple", 20)
-    lru_cache.insert_key_value("guava", 30)
+    lru_cache.put("mango", 10)
+    lru_cache.put("apple", 20)
+    lru_cache.put("guava", 30)
     print(lru_cache.most_recent_key())
 
-    lru_cache.insert_key_value("mango", 40)
+    lru_cache.put("mango", 40)
     print(lru_cache.most_recent_key())
 
-    order = lru_cache.get_value("mango")
+    order = lru_cache.get("mango")
     if order:
         print("Order of Mango is", order)
 
-    lru_cache.insert_key_value("banana", 20)
+    lru_cache.put("banana", 20)
 
-    if lru_cache.get_value("apple") is None:
+    print(lru_cache.cache)
+    if lru_cache.get("apple") is None:
         print("Apple doesn't exist")
 
-    if lru_cache.get_value("guava") is None:
+    if lru_cache.get("guava") is None:
         print("Guava doesn't exist")
 
-    if lru_cache.get_value("banana") is None:
+    if lru_cache.get("banana") is None:
         print("Banana doesn't exist")
 
-    if lru_cache.get_value("mango") is None:
+    if lru_cache.get("mango") is None:
         print("Mango doesn't exist")
